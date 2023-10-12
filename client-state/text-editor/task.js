4e93574a0024309164fd09c146fb7bc827a301ac
AVZapevalov <106755729+AVZapevalov@users.noreply.github.com>
2023-10-12 18:48:53 +0100

    text-editor-edit

--- client-state/text-editor/task.js
+++ client-state/text-editor/task.js
@@ -2,10 +2,6 @@ const editor = document.getElementById('editor');
 
 const savedText = localStorage.getItem('editorText');
 
-if (savedText) {
-    editor.value = savedText;
-}
-
 editor.addEventListener('input', () => {
     localStorage.setItem('editorText', editor.value);
 });
