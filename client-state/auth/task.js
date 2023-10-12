36ae842a10d3286db62755afb10bc3f9efbb9154
AVZapevalov <106755729+AVZapevalov@users.noreply.github.com>
2023-10-12 19:24:09 +0100

    auth

--- client-state/auth/task.js
+++ client-state/auth/task.js
@@ -1,31 +1,32 @@
 document.getElementById("signin__form").addEventListener("submit", (e) => {
   e.preventDefault();
 
-  let login = document.getElementsByName("login")[0].value;
-  let password = document.getElementsByName("password")[0].value;
+  let form = document.getElementById("signin__form");
+  let login = form.elements.login.value;
+  let password = form.elements.password.value;
 
   let xhr = new XMLHttpRequest();
   xhr.open("POST", "https://students.netoservices.ru/nestjs-backend/auth");
 
-  console.log(xhr.status);
-
   xhr.onload = function () {
     if (xhr.status === 201) {
       let response = JSON.parse(xhr.responseText);
+
       if (response.success) {
         let userId = response.user_id;
         localStorage.setItem("user_id", userId);
         showWelcomeBlock(userId);
+        form.reset();
       } else {
         alert("Неверный логин/пароль");
       }
     } else {
-      alert("Ошибка при отправке запроса");
+      alert("Ошибка при отправке запроса или сервер вернул ошибку");
     }
   };
 
-  let data = JSON.stringify({ login: login, password: password });
-  xhr.send(data);
+  let formData = new FormData(form);
+  xhr.send(formData);
 });
 
 function showWelcomeBlock(userId) {
